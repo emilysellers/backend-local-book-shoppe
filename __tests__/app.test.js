9d7caf7b3132036280d13197f2c16e7819fabb38
Emily Sellers <emilymelindasellers@gmail.com>
2022-11-07 16:41:09 -0800

    add test for GET /books, failing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,21 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('books routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('should return a list of books', async () => {
+    const res = await request(app).get('/books');
+    expect(res.body.length).toEqual(13);
+    const lonesomeDove = res.body.find((char) => char.number === 1985);
+    expect(lonesomeDove).toHaveProperty('title', 'Lonesome Dove');
+    expect(lonesomeDove).toHaveProperty('released', 1985);
   });
+
   afterAll(() => {
     pool.end();
   });
