e5cc63525ff68c4c9906e77ea7d61171c7cdf595
Emily Sellers <emilymelindasellers@gmail.com>
2022-11-08 18:15:53 -0800

    update GET /books/:id test, failing

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -22,12 +22,19 @@ describe('books routes', () => {
   it('/books/:id should return book and author', async () => {
     const res = await request(app).get('/books/1');
     expect(res.status).toBe(200);
-    // expect(res.body[0]).toEqual({
-    //   id: '1',
-    //   title: 'The Bluest Eye',
-    //   released: '1970',
-    //   authors: [{ id: '2', name: 'Toni Morrison' }], // author id and name
-    // });
+    expect(res.body).toEqual({
+      id: '1',
+      title: 'The Bluest Eye',
+      released: '1970',
+      authors: [
+        {
+          dob: 1931,
+          id: 2,
+          name: 'Toni Morrison',
+          pob: 'Lorain, OH, USA',
+        },
+      ],
+    });
   });
 
   afterAll(() => {
